211eb9882eede54111e002215fc149775a5d8eda
jxnblk <jacksonblack@gmail.com>
2016-02-13 18:53:16 -0500

    Use latest css-scss and add basscss-addons

--- build.js
+++ build.js
@@ -20,18 +20,44 @@ var modules = [
   'basscss-hide'
 ]
 
+var addons = [
+  'basscss-addons/modules/all',
+  'basscss-addons/modules/background-colors',
+  'basscss-addons/modules/background-images',
+  'basscss-addons/modules/border-colors',
+  'basscss-addons/modules/btn',
+  'basscss-addons/modules/btn-outline',
+  'basscss-addons/modules/btn-primary',
+  'basscss-addons/modules/colors',
+  'basscss-addons/modules/darken',
+  'basscss-addons/modules/forms',
+  'basscss-addons/modules/highlight-dark',
+  'basscss-addons/modules/highlight',
+  'basscss-addons/modules/input-range',
+  'basscss-addons/modules/lighten',
+  'basscss-addons/modules/media-object',
+  'basscss-addons/modules/progress',
+  'basscss-addons/modules/responsive-margin',
+  'basscss-addons/modules/responsive-padding'
+]
+
 // Build partials
-modules.forEach(function (m) {
+
+function buildPartial (m) {
   var css = '@import "' + m + '";'
 
   postcss([ postcssImport ])
     .process(css).then(function (result) {
       var scss = cssScss(result.css)
-      var filename = '_' + m.replace('basscss-', '') + '.scss'
+      var shortname = m.replace(/^basscss\-addons\/modules\/|^basscss\-/, '')
+      var filename = '_' + shortname + '.scss'
 
       fs.writeFileSync(path.join('scss', filename), scss)
     })
-})
+}
+
+modules.forEach(buildPartial)
+addons.forEach(buildPartial)
 
 // Build index
 var index = modules.map(function (m) {
