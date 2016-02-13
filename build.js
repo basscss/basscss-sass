74c359aa1fa22ff551672864dc9fbb351ac7dedf
jxnblk <jacksonblack@gmail.com>
2016-02-13 18:01:45 -0500

    Update build

--- build.js
+++ build.js
@@ -1,41 +1,42 @@
 // Compile Basscss source to scss syntax
 
-var fs = require('fs');
-var path = require('path');
-var cssscss = require('css-scss');
-var cssnext = require('cssnext');
-var basscss = require('basscss');
+var fs = require('fs')
+var path = require('path')
+var cssScss = require('css-scss')
+var postcss = require('postcss')
+var postcssImport = require('postcss-import')
 
-var modules = basscss.variables;
-modules = modules.concat(basscss.modules);
-modules = modules.concat(basscss.optional_modules);
-
-var cssnextOpts = {
-  features: {
-    customProperties: false,
-    customMedia: false,
-    calc: false,
-    colorFunction: false,
-    rem: false,
-  }
-};
+var modules = [
+  'basscss-type-scale',
+  'basscss-typography',
+  'basscss-layout',
+  'basscss-align',
+  'basscss-margin',
+  'basscss-padding',
+  'basscss-grid',
+  'basscss-flexbox',
+  'basscss-position',
+  'basscss-border',
+  'basscss-hide'
+]
 
 // Build partials
-modules.forEach(function(m) {
+modules.forEach(function (m) {
+  var css = '@import "' + m + '";'
 
-  var css = '@import "' + m + '";';
-  var imported = cssnext(css, cssnextOpts);
-  var scss = cssscss(imported);
-  var filename = '_' + m.replace('basscss-', '') + '.scss';
+  postcss([ postcssImport ])
+    .process(css).then(function (result) {
+      var scss = cssScss(result.css)
+      var filename = '_' + m.replace('basscss-', '') + '.scss'
 
-  fs.writeFileSync(filename, scss);
-
-});
+      fs.writeFileSync(path.join('scss', filename), scss)
+    })
+})
 
 // Build index
-var index = modules.map(function(m) {
-  return '@import "' + m.replace(/^basscss\-/,'') + '";';
-});
+var index = modules.map(function (m) {
+  return '@import "' + m.replace(/^basscss\-/,'') + '";'
+})
 
-fs.writeFileSync('basscss.scss', index.join('\n'));
+fs.writeFileSync(path.join('scss', 'basscss.scss'), index.join('\n'))
 
--- test/test.js
+++ test/test.js
@@ -5,29 +5,35 @@ var assert = require('assert')
 var sass = require('node-sass')
 
 var filenames = fs.readdirSync(path.join(__dirname, '../scss'))
-  .filter(function(name) {
+  .filter(function (name) {
     return name.match(/\.scss$/)
   })
 
-var files = filenames.map(function(filename) {
-  var src = fs.readFileSync(path.join(__dirname, '../' + filename), 'utf8')
+var files = filenames.map(function (filename) {
+  var src = fs.readFileSync(path.join(__dirname, '../scss', filename), 'utf8')
   return { src: src, name: filename }
 })
 
+var index = {
+  name: 'index',
+  src: fs.readFileSync(path.join(__dirname, '../scss/basscss.scss'), 'utf8')
+}
 
-describe('basscss-sass', function() {
-
-  function renderSass(file) {
-    it('should compile scss for ' + file.name, function() {
-      var src = defaults + '\n' + file.src
-      assert.doesNotThrow(function() {
-        sass.renderSync({ data: src })
+describe('basscss-sass', function () {
+  function renderSass (file) {
+    it('should compile scss for ' + file.name, function () {
+      assert.doesNotThrow(function () {
+        sass.renderSync({
+          data: file.src,
+          includePaths: [
+            path.join(__dirname, '../scss')
+          ]
+        })
       })
     })
   }
 
   files.forEach(renderSass)
   renderSass(index)
-
 })
 
